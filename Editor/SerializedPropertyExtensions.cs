33ffdfead62f64500e138e3044815f82e71a0f84
Enes Sahin (Potato) <enes@herogamestudio.com>
2023-02-19 17:00:37 +0300

    # More SerializedProperty extension methods

--- Editor/SerializedPropertyExtensions.cs
+++ Editor/SerializedPropertyExtensions.cs
@@ -7,190 +7,218 @@ using System.Collections.Generic;
 
 namespace EnesShahn.Extensions
 {
-    public static class SerializedPropertyExtensions
-    {
-        private static string pattern = @".*\[(\d*)\]";
-        private static Regex rx = new Regex(pattern);
+	public static class SerializedPropertyExtensions
+	{
+		private static string pattern = @".*\[(\d*)\]";
+		private static Regex rx = new Regex(pattern);
 
-        public static object GetObjectValue(this SerializedProperty property)
-        {
-            var iterator = GetSerializedPropertyEnumerator(property);
-            object currentObject = null;
-            while (iterator.MoveNext())
-            {
-                currentObject = iterator.Current.Value;
-            }
-            return currentObject;
-        }
-        public static T GetObjectValue<T>(this SerializedProperty property)
-        {
-            return (T)GetObjectValue(property);
-        }
-        public static Type GetPropertyFieldType(this SerializedProperty property)
-        {
-            var iterator = GetSerializedPropertyEnumerator(property);
-            while (iterator.MoveNext())
-            {
-                if (iterator.Current.Property.propertyPath == property.propertyPath)
-                    return iterator.Current.FieldType;
-            }
-            return null;
-        }
+		public static object GetObjectValue(this SerializedProperty property)
+		{
+			var iterator = GetSerializedPropertyEnumerator(property);
+			object currentObject = null;
+			while (iterator.MoveNext())
+			{
+				currentObject = iterator.Current.Value;
+			}
+			return currentObject;
+		}
+		public static T GetObjectValue<T>(this SerializedProperty property)
+		{
+			return (T)GetObjectValue(property);
+		}
+		public static Type GetPropertyFieldType(this SerializedProperty property)
+		{
+			var iterator = GetSerializedPropertyEnumerator(property);
+			while (iterator.MoveNext())
+			{
+				if (iterator.Current.Property.propertyPath == property.propertyPath)
+					return iterator.Current.FieldType;
+			}
+			return null;
+		}
 
-        public static bool IsAnyAncestorOfType(this SerializedProperty property, params Type[] checkTypes)
-        {
-            var iterator = GetSerializedPropertyEnumerator(property);
+		public static bool IsAnyAncestorOfType(this SerializedProperty property, params Type[] checkTypes)
+		{
+			var propValue = property.GetObjectValue();
+			var iterator = GetSerializedPropertyEnumerator(property);
+			while (iterator.MoveNext())
+			{
+				if (iterator.Current.Value.Equals(propValue)) break;
+				var currentObjectTypeCheck = iterator.Current.Value.GetType().GetGenericTypeDefinitionIfGeneric();
+				foreach (var checkType in checkTypes)
+				{
+					if (currentObjectTypeCheck == checkType)
+					{
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+		public static bool IsAnyAncestorArray(this SerializedProperty property)
+		{
+			var propValue = property.GetObjectValue();
+			var iterator = GetSerializedPropertyEnumerator(property);
+			bool isArray = false;
+			while (iterator.MoveNext())
+			{
+				if (iterator.Current.Value.Equals(propValue)) break;
+				var currentObjectTypeCheck = iterator.Current.Value.GetType().GetGenericTypeDefinitionIfGeneric();
+				if (currentObjectTypeCheck.IsArray)
+				{
+					isArray = true;
+				}
+			}
+			return isArray;
+		}
+		public static bool IsAnyArrayAncestorNotOfType(this SerializedProperty property, Type checkType)
+		{
+			var iterator = GetSerializedPropertyEnumerator(property);
+			while (iterator.MoveNext())
+			{
+				var currentObjectType = iterator.Current.Value.GetType().GetGenericTypeDefinitionIfGeneric();
 
-            while (iterator.MoveNext())
-            {
-                var currentObjectTypeCheck = iterator.Current.Value.GetType().GetGenericTypeDefinitionIfGeneric();
-                foreach (var checkType in checkTypes)
-                {
-                    if (currentObjectTypeCheck == checkType)
-                    {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
-        public static bool IsAnyArrayAncestorNotOfType(this SerializedProperty property, Type checkType)
-        {
-            var iterator = GetSerializedPropertyEnumerator(property);
-            while (iterator.MoveNext())
-            {
-                var currentObjectType = iterator.Current.Value.GetType().GetGenericTypeDefinitionIfGeneric();
+				if (currentObjectType == checkType)
+				{
+					iterator.MoveNext();
+					continue;
+				}
 
-                if (currentObjectType == checkType)
-                {
-                    iterator.MoveNext();
-                    continue;
-                }
+				if (currentObjectType == typeof(List<>) || currentObjectType.IsArray)
+					return true;
+			}
 
-                if (currentObjectType == typeof(List<>) || currentObjectType.IsArray)
-                    return true;
-            }
+			return false;
+		}
+		public static SerializedProperty GetParentSerializedProperty(this SerializedProperty property)
+		{
+			var propValue = property.GetObjectValue();
+			var iterator = GetSerializedPropertyEnumerator(property);
+			SerializedProperty parentSP = null;
+			while (iterator.MoveNext())
+			{
+				if (iterator.Current.Value.Equals(propValue)) break;
+				parentSP = iterator.Current.Property;
+			}
+			return parentSP;
+		}
+		public static IEnumerator<SPInfo> GetSerializedPropertyEnumerator(this SerializedProperty property)
+		{
+			SerializedObject serializedObject = property.serializedObject;
+			UnityEngine.Object targetObject = serializedObject.targetObject;
+			var bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
 
-            return false;
-        }
+			string[] propPathSplit = property.propertyPath.Split('.');
 
-        public static IEnumerator<SPInfo> GetSerializedPropertyEnumerator(this SerializedProperty property)
-        {
-            SerializedObject serializedObject = property.serializedObject;
-            UnityEngine.Object targetObject = serializedObject.targetObject;
-            var bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+			Type containerObjectType = targetObject.GetType();
+			FieldInfo currentObjectFieldInfo = containerObjectType.GetField(propPathSplit[0], bindingFlags);
+			object currentObject = currentObjectFieldInfo.GetValue(targetObject);
+			SerializedProperty currentProperty = serializedObject.FindProperty(propPathSplit[0]);
 
-            string[] propPathSplit = property.propertyPath.Split('.');
+			yield return new SPInfo(currentProperty.Copy(), currentObject, currentObjectFieldInfo.FieldType);
+			for (int i = 1; i < propPathSplit.Length; i++)
+			{
+				var currentObjectType = currentObject.GetType();
+				Type fieldType = null;
+				if (propPathSplit[i].Equals("Array"))
+				{
+					i++;
+					var pathRegexMatch = rx.Match(propPathSplit[i]);
+					int index = Convert.ToInt32(pathRegexMatch.Groups[1].Value);
 
-            Type containerObjectType = targetObject.GetType();
-            FieldInfo currentObjectFieldInfo = containerObjectType.GetField(propPathSplit[0], bindingFlags);
-            object currentObject = currentObjectFieldInfo.GetValue(targetObject);
-            SerializedProperty currentProperty = serializedObject.FindProperty(propPathSplit[0]);
+					Array array = null;
+					if (currentObjectType.IsGenericType && currentObjectType.GetGenericTypeDefinition() == typeof(List<>))
+					{
+						fieldType = currentObjectType;
+						currentObjectFieldInfo = currentObjectType.GetField("_items", bindingFlags);
+						array = (Array)currentObjectFieldInfo.GetValue(currentObject);
+					}
+					else if (currentObjectType.IsArray)
+					{
+						fieldType = currentObject.GetType();
+						array = (Array)currentObject;
+					}
 
-            yield return new SPInfo(currentProperty.Copy(), currentObject, currentObjectFieldInfo.FieldType);
-            for (int i = 1; i < propPathSplit.Length; i++)
-            {
-                var currentObjectType = currentObject.GetType();
-                Type fieldType = null;
-                if (propPathSplit[i].Equals("Array"))
-                {
-                    i++;
-                    var pathRegexMatch = rx.Match(propPathSplit[i]);
-                    int index = Convert.ToInt32(pathRegexMatch.Groups[1].Value);
+					if (array == null)
+					{
+						UnityEngine.Debug.Log("Miserable failure");
+					}
 
-                    Array array = null;
-                    if (currentObjectType.IsGenericType && currentObjectType.GetGenericTypeDefinition() == typeof(List<>))
-                    {
-                        fieldType = currentObjectType;
-                        currentObjectFieldInfo = currentObjectType.GetField("_items", bindingFlags);
-                        array = (Array)currentObjectFieldInfo.GetValue(currentObject);
-                    }
-                    else if (currentObjectType.IsArray)
-                    {
-                        fieldType = currentObject.GetType();
-                        array = (Array)currentObject;
-                    }
+					currentProperty = currentProperty.GetArrayElementAtIndex(index);
+					currentObject = array.GetValue(index);
+				}
+				else
+				{
+					currentProperty = currentProperty.FindPropertyRelative(propPathSplit[i]);
+					currentObjectFieldInfo = currentObjectType.GetField(propPathSplit[i], bindingFlags);
+					fieldType = currentObjectFieldInfo.FieldType;
+					currentObject = currentObjectFieldInfo.GetValue(currentObject);
+				}
 
-                    if (array == null)
-                    {
-                        UnityEngine.Debug.Log("Miserable failure");
-                    }
+				yield return new SPInfo(currentProperty.Copy(), currentObject, fieldType);
+			}
+			yield break;
+		}
 
-                    currentProperty = currentProperty.GetArrayElementAtIndex(index);
-                    currentObject = array.GetValue(index);
-                }
-                else
-                {
-                    currentProperty = currentProperty.FindPropertyRelative(propPathSplit[i]);
-                    currentObjectFieldInfo = currentObjectType.GetField(propPathSplit[i], bindingFlags);
-                    fieldType = currentObjectFieldInfo.FieldType;
-                    currentObject = currentObjectFieldInfo.GetValue(currentObject);
-                }
+		public static bool HasAttribute(this SerializedProperty property, Type attributeType)
+		{
+			BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+			var members = property.serializedObject.targetObject.GetType().GetMember(property.propertyPath, flags);
 
-                yield return new SPInfo(currentProperty.Copy(), currentObject, fieldType);
-            }
-            yield break;
-        }
+			foreach (var member in members)
+			{
+				if (member.CustomAttributes.Count() == 0) continue;
+				var attribute = member.GetCustomAttribute(attributeType);
+				if (attribute == null) continue;
+				if (member.Name != property.propertyPath) continue;
+				return true;
+			}
+			return false;
+		}
+		public static Attribute GetAttribute(this SerializedProperty property, Type attributeType)
+		{
+			BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+			var members = property.serializedObject.targetObject.GetType().GetMember(property.propertyPath, flags);
 
-        public static bool HasAttribute(this SerializedProperty property, Type attributeType)
-        {
-            BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
-            var members = property.serializedObject.targetObject.GetType().GetMember(property.propertyPath, flags);
+			foreach (var member in members)
+			{
+				if (member.CustomAttributes.Count() == 0) continue;
+				var attribute = member.GetCustomAttribute(attributeType);
+				if (attribute == null) continue;
+				if (member.Name != property.propertyPath) continue;
+				return attribute;
+			}
+			return null;
+		}
 
-            foreach (var member in members)
-            {
-                if (member.CustomAttributes.Count() == 0) continue;
-                var attribute = member.GetCustomAttribute(attributeType);
-                if (attribute == null) continue;
-                if (member.Name != property.propertyPath) continue;
-                return true;
-            }
-            return false;
-        }
-        public static Attribute GetAttribute(this SerializedProperty property, Type attributeType)
-        {
-            BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
-            var members = property.serializedObject.targetObject.GetType().GetMember(property.propertyPath, flags);
+		public static void SetChildrenExpand(this SerializedProperty property, bool isExpanded)
+		{
+			var parent = property.Copy();
+			parent.isExpanded = isExpanded;
+			var childIterator = parent.Copy();
+			if (childIterator.Next(true))
+			{
+				do
+				{
+					SetChildrenExpand(childIterator.Copy(), isExpanded);
+				}
+				while (childIterator.Next(false));
+			}
+		}
 
-            foreach (var member in members)
-            {
-                if (member.CustomAttributes.Count() == 0) continue;
-                var attribute = member.GetCustomAttribute(attributeType);
-                if (attribute == null) continue;
-                if (member.Name != property.propertyPath) continue;
-                return attribute;
-            }
-            return null;
-        }
+	}
 
-        public static void SetChildrenExpand(this SerializedProperty property, bool isExpanded)
-        {
-            var parent = property.Copy();
-            parent.isExpanded = isExpanded;
-            var childIterator = parent.Copy();
-            if (childIterator.Next(true))
-            {
-                do
-                {
-                    SetChildrenExpand(childIterator.Copy(), isExpanded);
-                }
-                while (childIterator.Next(false));
-            }
-        }
+	public class SPInfo
+	{
+		public SerializedProperty Property;
+		public object Value;
+		public Type FieldType;
 
-    }
-
-    public class SPInfo
-    {
-        public SerializedProperty Property;
-        public object Value;
-        public Type FieldType;
-
-        public SPInfo(SerializedProperty property, object value, Type type)
-        {
-            Property = property;
-            Value = value;
-            FieldType = type;
-        }
-    }
+		public SPInfo(SerializedProperty property, object value, Type type)
+		{
+			Property = property;
+			Value = value;
+			FieldType = type;
+		}
+	}
 }
